d7d5e38e0e5933d0bf757e09afe5273b81ef9d84
Jacques Berger <jacques.berger@code3.ca>
2024-04-06 12:38:20 -0400

    Ajouter une table d'interaction

--- db/scripts/minimal-0001.sql
+++ db/scripts/minimal-0001.sql
@@ -2,3 +2,10 @@ create table entreprise (
   id integer primary key,
   nom varchar
 );
+
+create table interaction (
+  id integer primary key,
+  description varchar,
+  moment varchar,
+  entreprise_id integer foreign key references entreprise(id)
+);
