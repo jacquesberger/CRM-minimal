a5d40b5544765cbab6ca76fed2414cd32202510f
Jacques Berger <jacques.berger@code3.ca>
2024-04-08 23:48:32 -0400

    Changer un commentaire de licence

--- index.py
+++ index.py
@@ -1,4 +1,4 @@
-# Copyright 2024 <Votre nom et code permanent>
+# Copyright 2024 Jacques Berger
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
